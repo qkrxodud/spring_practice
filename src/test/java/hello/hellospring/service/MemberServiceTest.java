5630808f52cfff88725fa6aa6605672119e17604
qkrxodud00 <qkrxodud00@gmail.com>
2022-03-03 18:18:02 +0900

    *TDD  - 간단한 TDD 사용방법 진행중.  - given, when, then을 이용해서 객체를 만들면 쉽다.

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,41 @@
+package hello.hellospring.service;
+import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class MemberServiceTest {
+    MemberService memberService = new MemberService();
+
+    @Test
+    void 회원가입() {
+        //given 이런 상황을 주어줬을대
+        Member member = new Member();
+        member.setName("number1");
+
+        //when 이것을 실행 해준다.
+        Long saveId = memberService.join(member);
+
+        //then 결과값이 나온다.
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void 중복_회원_예외() {
+
+    }
+
+    @Test
+    void validateDuplicateMember() {
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
