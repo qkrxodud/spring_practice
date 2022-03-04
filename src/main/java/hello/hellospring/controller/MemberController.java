f1c47089aa73857147d84782848f2c67ce67e515
qkrxodud00 <qkrxodud00@gmail.com>
2022-03-04 14:07:52 +0900

    *회원가입 & 조회  - post 방식으로 넘어 올때는 PostMapping로 객체를 먼저 만든 후 해당하는 객체를 입력 받아 사용한다.  - 회원가입 조회시 list의 값을 출력할때 html 부분에서 tymeleaf가 each 를 통해서 자동으로 완성 시켜준다.

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,12 +1,37 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class MemberController {
 
     private final MemberService memberService;
 
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm memberForm) {
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("members")
+    public String List(Model model) {
+        model.addAttribute("members", memberService.findMembers());
+        return "members/memberList";
     }
 }
