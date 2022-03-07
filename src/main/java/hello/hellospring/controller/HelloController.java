58995966b99b1c5e13501fa3ef52b79919a9f1a6
qkrxodud00 <qkrxodud00@gmail.com>
2022-03-07 13:16:56 +0900

    *스프링 웹 개발 기초  - https://cozing.tistory.com/56?category=1035517

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,6 +19,26 @@ public class HelloController {
     public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
         model.addAttribute("name", name);
         return "hello-mvc";
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public HelloApi helloApi(@RequestParam("name") String name) {
+        HelloApi helloApi = new HelloApi();
+        helloApi.setName(name);
+        return helloApi;
+    }
+
+    static class HelloApi {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
 }
